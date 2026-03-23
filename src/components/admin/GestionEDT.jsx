3c54bd7d3bc5dd6a3c5d0d3158e1ef1cc4a1f564
mad1409 <mad1409@github.com>
2026-03-23 00:30:27 +0000

    fix: useEffect import GestionEDT

--- src/components/admin/GestionEDT.jsx
+++ src/components/admin/GestionEDT.jsx
@@ -1,5 +1,5 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { api } from "../../api";
 import SearchSelect from "../shared/SearchSelect";
 import { analyserConflits } from "../../utils/conflits";
